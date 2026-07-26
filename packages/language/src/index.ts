50fd82583a1216e6309ead27410f803e810996cd
Ninou09 <nadjibrais33@gmail.com>
2026-07-26 20:04:12 +0300

    feat(language): export placement engine

---
+++ packages/language/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./assessment";
+export * from "./constants";
+export * from "./permissions";
+export * from "./recommendations";
+export * from "./scoring";
+export * from "./types";
+export * from "./validators";
