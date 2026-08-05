7e7a9632a10b29bee35db52fd050bd815b6f6fec
Ninou09 <nadjibrais33@gmail.com>
2026-08-05 16:17:59 +0300

    Allow optimized Sanity programme images

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,15 @@ import { securityHeaders } from '@luminol/config/security-headers';
 const nextConfig: NextConfig = {
   transpilePackages: ['@luminol/ui', '@luminol/validation'],
   serverExternalPackages: ['@prisma/client'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/images/**',
+      },
+    ],
+  },
   outputFileTracingIncludes: {
     '/*': [
       '../../node_modules/.pnpm/@prisma+client*/node_modules/.prisma/client/**/*',
