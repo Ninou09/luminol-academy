254d27365b3bb6033cf42ed4ca26e9aacbf439e1
Ninou09 <nadjibrais33@gmail.com>
2026-08-17 14:11:56 +0300

    Export Milestone 17 analytics contracts

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -1,8 +1,12 @@
+export * from './learning-outcomes';
+
 export type AnalyticsEvent = {
   name: string;
   properties?: Readonly<Record<string, string | number | boolean>>;
 };
+
 export interface AnalyticsAdapter {
   track(event: AnalyticsEvent): void | Promise<void>;
 }
+
 export const noopAnalytics: AnalyticsAdapter = { track: () => undefined };
