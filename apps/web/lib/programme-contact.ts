abfae3a929723b3a89a592d2ae4b6e95156cb5f4
Ninou09 <nadjibrais33@gmail.com>
2026-09-04 01:44:48 +0300

    Add programme-aware contact href helper

---
+++ apps/web/lib/programme-contact.ts
@@ -0,0 +1,8 @@
+import { localizeHref, type Locale } from '@luminol/localization';
+
+export function buildProgrammeContactHref(locale: Locale, programmeSlug: string) {
+  const contactHref = localizeHref(locale, '/contact');
+  const params = new URLSearchParams({ programme: programmeSlug });
+
+  return `${contactHref}?${params.toString()}`;
+}
